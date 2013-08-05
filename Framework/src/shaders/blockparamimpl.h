191d4ae992befd4cd416b3da1efc38d96310c1ec
mark <github@alternity.org.uk>
2013-08-05 13:55:11 +0100

    a better shader infrastructure (hopefully)

--- Framework/src/shaders/blockparamimpl.h
+++ Framework/src/shaders/blockparamimpl.h
@@ -4,16 +4,19 @@
 #include <string>
 #include <glm/glm.hpp>
 #include <gl_loader/gl_loader.h>
-#include <shaders/shaderparam.h>
+#include <shaders/shaderparaminterface.h>
 #include <buffers/shaderuniformbuffer.h>
 
 class ShaderBlockParam : public IShaderParam
 {
 public:
 	ShaderBlockParam(const std::string& name, GLenum type, Buffers::ShaderUniformBuffer buffer, size_t offset)
-		: changed(false), name(name), type(type), offset(offset), buffer(buffer)
+		: changed(false),
+			name(name), type(type), offset(offset), buffer(buffer)
 	{
 	}
+
+	virtual const std::string& GetName() const;
 
 	virtual void Set(float value);
 	virtual void Set(const glm::vec2& value);
@@ -32,6 +35,8 @@ public:
 	virtual glm::mat4 GetMat4() const;
 
 	virtual void Apply();
+
+	void CacheValue(const void* const value, size_t size);
 
 	bool changed;
 	const std::string name;
